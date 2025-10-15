b53e3bec617acc49cc73337f9576f898b9534a97
Jyunyu-kollectin <jyunyu@kollectin.com>
2025-10-15 15:39:01 +0800

    Add test cd-2

--- DevOpsDemo/Controllers/ProductsControllers.cs
+++ DevOpsDemo/Controllers/ProductsControllers.cs
@@ -12,11 +12,7 @@ public class ProductsController : ControllerBase
             new { Id = 2, Name = "Mouse", Price = 25 }
         });
 
-    [HttpGet]
-    public IActionResult GetAll() =>
-        Ok(new[]
-        {
-            new { Id = 1, Name = "Laptop", Price = 1200 },
-            new { Id = 2, Name = "Mouse", Price = 25 }
-        });
+    [HttpGet("test")]
+    public IActionResult Test() =>
+        Ok("CI/CD test successful!");
 }
